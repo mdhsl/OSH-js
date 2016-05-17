c424595adfe0f1cbbe9400c48f3fe7a2ac38d30d
Mathieu dhainaut <mathieu.dhainaut@gmail.com>
2016-05-17 23:00:46 +0200

    fix default cesium altitude

--- src/plugins/cesium/osh-UI-CesiumView.js
+++ src/plugins/cesium/osh-UI-CesiumView.js
@@ -24,7 +24,7 @@ OSH.UI.CesiumView = Class.create(OSH.UI.View,{
     var east = Cesium.Math.toRadians(0);
     var north = Cesium.Math.toRadians(0);
     var rect = new Cesium.Rectangle(west, south, east, north);
-    this.camPos = new Cesium.HeadingPitchRange(Cesium.Math.toRadians(-10), Cesium.Math.toRadians(-30), 800.0);
+    this.camPos = new Cesium.HeadingPitchRange(Cesium.Math.toRadians(-10), Cesium.Math.toRadians(-30), 5000.0);
 
     var targetEntities = [];
 
